ac1b592449606d270d8406cd79241fccf0fbbc4a
allesonsales <allesonsales@gmail.com>
2025-06-10 12:45:51 -0300

    vite criado

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lacrei/",
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
